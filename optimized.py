7f305503caf155b75720f5e84a8c8a47581a4668
Thomas Creusot <th.creusot@gmail.com>
2022-08-11 11:07:42 +0200

    Functional and clean version of optimized.py

--- optimized.py
+++ optimized.py
@@ -1,28 +1,35 @@
 """
 Le programme doit fournir une réponse en moins d'une seconde.
-Nous n'avons plus besoin d'explorer toutes les combinaisons possibles, ce qui, à mon avis, devrait accélérer l'algorithme.
+Nous n'avons plus besoin d'explorer toutes les combinaisons possibles, ce qui, à mon avis, devrait
+accélérer l'algorithme.
 """
 
 """
-Two ideas : 
-Idea A : select best investment based on its benefice
-Idea B : select best investment based on its benefice/cost ratio
+Ideas :
+Idea A : select best investment based on its benefice(euros)
+Idea B : select best investment based on its benefice(euros)/cost ratio = benefice(%)
+Idea C : select best investment based on benefit(%) * benefit(euros) = benefit(%)*benefit(%)*cost
+>>>in fact: better results with benefit(percent) on Sienna data
 
-Choice type of algorithm 
+Idea D : select best investment based on benefit(%) / cost
+>>>DivisionZero error ; if (0.1 + cost) >>> not good results
+
+Choice type of algorithm
 >>>https://fr.wikipedia.org/wiki/Algorithme_de_tri#Comparaison_des_algorithmes
 
-Mes criteres: 
+Mes criteres:
 -Je sors tous les algos instables
 
-
 4 equivalences + 2 cas qui se démarquent
-Tri par insertion :     temporelle : entre n et n² ;    spatiale : 1 ;      interessant mais je pense que n² sera trop long
-Tri à bulles :          temporelle : entre n et n² ;    spatiale : 1 ;      interessant mais je pense que n² sera trop long
-Tri cockail :           temporelle : entre n et n² ;    spatiale : 1 ;      interessant mais je pense que n² sera trop long
-Tri pair-impair :       temporelle : entre n et n² ;    spatiale : 1 ;      interessant mais je pense que n² sera trop long
+Tri par insertion :     temporelle : entre n et n² ;    spatiale : 1 ;
+Tri à bulles :          temporelle : entre n et n² ;    spatiale : 1 ;
+Tri cockail :           temporelle : entre n et n² ;    spatiale : 1 ;
+Tri pair-impair :       temporelle : entre n et n² ;    spatiale : 1 ;
+>>>interessants mais je pense que n² sera trop long
 
 Tri arboresence :  :    temporelle : nlogn ;            spatiale : n ;  interessant
-> Cependant, il est moins efficace car il nécessite de construire une structure de données complexe alors que le tri rapide est un tri en place. Il n'est donc pas utilisé en pratique. 
+> Cependant, il est moins efficace car il nécessite de construire une structure de données complexe
+alors que le tri rapide est un tri en place. Il n'est donc pas utilisé en pratique. 
  > écarté
 
 Timsort :               temporelle : nlogn ;            spatiale : n ;  interessant
@@ -36,44 +43,65 @@ Tri fusion :            temporelle : entre n et nlogn ; spatiale : n ;  interess
 > + décrit et pseudo code disponible
 
 Partons sur Timsort dans un premier temps.
+Tri fusion choisi pour sa simplicité
 """
 
+"""Le tri fusion se décrit naturellement sur des listes et c'est sur de telles structures qu'il est
+à la fois le plus simple et le plus rapide.
+Le principe de l'algorithme de tri fusion repose sur cette observation : le plus petit élément
+de la liste à construire est soit le plus petit élément de la première liste, soit le plus petit
+élément de la deuxième liste. Ainsi, on peut construire la liste élément par élément en retirant
+tantôt le premier élément de la première liste, tantôt le premier élément de la deuxième liste
+"""
+
+
 import csv
+from time import time
 
 
 MAXIMUM_EXPENDITURE = 500
 
 available_money = MAXIMUM_EXPENDITURE
-
 actions_parameters = []  # will contain all actions and their relative information 
 
-def getActionParametersFromACsvFile(csvFileName):
+
+def getActionParametersFromACsvFile(csvFileName, headerActionName,
+                                    headerActionCost, headerActionProfitPercent):
     """Opens a CSV file and gets parameters and prices of actions"""
-    # CSV package ; method DictReader() : Cette méthode sait que la première ligne est un en-tête et 
-    # sauvegarde les autres lignes en tant que dictionnaires. Chaque clé est un nom de colonne et la 
-    # valeur est la valeur de la colonne
+    # CSV package ; method DictReader() : Cette méthode sait que la première ligne est un en-tête
+    # et sauvegarde les autres lignes en tant que dictionnaires. Chaque clé est un nom de colonne
+    # et la valeur est la valeur de la colonne
 
     with open(csvFileName) as csv_file:
         reader = csv.DictReader(csv_file, delimiter=',')
         for line in reader:
             # actions_parameters filling
-            action_name = line['Action-#']
-            action_cost = int(line['Cout_par_action_(en_euros)'])
-            action_benefit_percent = float(line['Bénéfice_(après_2_ans)'])
+            action_name = line[headerActionName]
+            action_cost = float(line[headerActionCost])
+            action_benefit_percent = float(line[headerActionProfitPercent])/100
             action_benefit_euros = action_cost * action_benefit_percent
-            action_benefit_euros_cost_ratio = action_benefit_euros / action_cost
+
+            # DECISION ABOUT SORTING
+            # 'action_benefit_percent' or 'action_benefit_percent * action_benefit_euros'
+            sorting_criteria = action_benefit_percent 
             
-            #Dictionnary
-            #actions_parameters[line['Action-#']] = {'cost': action_cost, 'benefit_euros' : action_benefit_euros, 'benefit_euros_cost_ratio' : action_benefit_euros_cost_ratio}
-            #List
-            action_parameter = [action_benefit_euros_cost_ratio, action_benefit_euros, action_name, action_cost] #action_benefit_euros_cost_ratio first place
+            # Dictionnary solution
+            #actions_parameters[line['Action-#']] = {'cost': action_cost, 
+            #'benefit_euros' : action_benefit_euros, 
+            #'benefit_euros_cost_ratio' : action_benefit_euros_cost_ratio}
+
+            # List solution
+            # Note : action_benefit_euros_benefit_percent first place
+            # PLESE DO NOT CHANGE THE ORDER (see main() function hereunder)
+            action_parameter = [sorting_criteria, action_benefit_euros, action_name, 
+                                action_cost, action_benefit_percent] 
             actions_parameters.append(action_parameter)
 
-    # print(actions_parameters["Action-1"])
 
-
-def FusionPart_sortingAlgorithm_by_benefit(halfTablePartA, halfTablePartB, indexOfParameterToSortBy):
-    # https://www.delftstack.com/fr/howto/python/merge-sort-in-python/
+def FusionPart_sortingAlgorithm(halfTablePartA, halfTablePartB, 
+                                           indexOfParameterToSortBy):
+    """'Fusion' part of the fusion algorithm"""
+    # Source: https://www.delftstack.com/fr/howto/python/merge-sort-in-python/
 
     if not len(halfTablePartA) or not len(halfTablePartB):
         return halfTablePartA or halfTablePartB
@@ -82,20 +110,25 @@ def FusionPart_sortingAlgorithm_by_benefit(halfTablePartA, halfTablePartB, index
     i = 0
     j = 0
     
-    #tant que la finalSortedTable ne contient pas tous les éléments à trier 
+    # Tant que la finalSortedTable ne contient pas tous les éléments à trier 
     while (len(finalSortedTable) < len(halfTablePartA) + len(halfTablePartB)):
 
-        #le premier 'élément de liste qui n'a pas encore été trié (i et j incrémentés)' le plus petit est gardé dans la table finale
-        if halfTablePartA[i][indexOfParameterToSortBy] > halfTablePartB[j][indexOfParameterToSortBy]: #halfTablePartA[i] < halfTablePartB[j]: est la ligne originelle, j'ajoute [...] pour trier selon un parametre ou un autre (prix, rendement, etc)
+        # Le premier 'élément de liste qui n'a pas encore été trié (i et j incrémentés)' 
+        # le plus petit/grand est gardé dans la table finale (le plus grand pour un tri 
+        # décroissant)
+        
+        # halfTablePartA[i] < halfTablePartB[j]: est la ligne originelle, j'ajoute [...] pour trier 
+        # selon un parametre ou un autre (prix, rendement, etc)
+        if halfTablePartA[i][indexOfParameterToSortBy] > \
+        halfTablePartB[j][indexOfParameterToSortBy]: 
             finalSortedTable.append(halfTablePartA[i])
             i+= 1
         else:
             finalSortedTable.append(halfTablePartB[j])
             j+= 1
 
-
-
-        #si toute la table A ou tout la table B a été triée , alors on fusionne la table finale avec la table qui n'a pas été triée
+        # Si toute la table A ou tout la table B a été triée , alors on fusionne la table finale
+        # avec la table qui n'a pas été triée
         if i == len(halfTablePartA) or j == len(halfTablePartB):
             finalSortedTable.extend(halfTablePartA[i:] or halfTablePartB[j:])
             break
@@ -103,60 +136,61 @@ def FusionPart_sortingAlgorithm_by_benefit(halfTablePartA, halfTablePartB, index
     return finalSortedTable
 
 
-
-def sortingFusionPart_sortingAlgorithm_by_benefit(tableToSort, indexOfParameterToSortBy):
-    # https://www.delftstack.com/fr/howto/python/merge-sort-in-python/
+def sortingFusionPart_sortingAlgorithm(tableToSort, indexOfParameterToSortBy):
+    """'sortingFusion' part of the fusion algorithm"""
+    # Source : https://www.delftstack.com/fr/howto/python/merge-sort-in-python/
 
     tableToSortLenght = len(tableToSort)
-    if tableToSortLenght <= 1: #2 in the source, but does not sort totally
+    if tableToSortLenght <= 1: #2 in the source, but does not sort totally; I replace with 1
         return tableToSort
     else:
-        halfTablePartA = sortingFusionPart_sortingAlgorithm_by_benefit(tableToSort[:int(tableToSortLenght/2)], indexOfParameterToSortBy)
-        halfTablePartB = sortingFusionPart_sortingAlgorithm_by_benefit(tableToSort[int(tableToSortLenght/2):], indexOfParameterToSortBy)
-        return FusionPart_sortingAlgorithm_by_benefit(halfTablePartA, halfTablePartB, indexOfParameterToSortBy)
+        halfTablePartA = sortingFusionPart_sortingAlgorithm(
+            tableToSort[:int(tableToSortLenght/2)], indexOfParameterToSortBy
+            )
+        halfTablePartB = sortingFusionPart_sortingAlgorithm(
+            tableToSort[int(tableToSortLenght/2):], indexOfParameterToSortBy
+            )
+        return FusionPart_sortingAlgorithm(halfTablePartA, halfTablePartB, 
+                                                      indexOfParameterToSortBy)
 
 
 
 
-"""Le tri fusion se décrit naturellement sur des listes et c'est sur de telles structures qu'il est à la fois le plus simple et le plus rapide. """
+def main(csvFileName, headerActionName, headerActionCost, headerActionProfitPercent):
+    """Main function of the program"""
 
-"""Le principe de l'algorithme de tri fusion repose sur cette observation : le plus petit élément
- de la liste à construire est soit le plus petit élément de la première liste, soit le plus petit
-  élément de la deuxième liste. Ainsi, on peut construire la liste élément par élément en
-   retirant tantôt le premier élément de la première liste, tantôt le premier élément de
-    la deuxième liste """
-
-
-
-
-"""
-def sortingAlgorithm_by_benefitCostRatio(actions_parameters):
-    for action in actions_parameters:
-        print(actions_parameters[action])
-"""
-
-
-def main():
-    getActionParametersFromACsvFile('part1_actions.csv')
-    sorted_actions = sortingFusionPart_sortingAlgorithm_by_benefit(actions_parameters, indexOfParameterToSortBy = 2) #ON CHOISIR PAR QUOi TRIER ICI
-    print(sorted_actions)
-
-    global MAXIMUM_EXPENDITURE
-
+    start_total_time = time()
+    global available_money
     bought_actions = []
     total_benefit=0
+
+
+    print("PREDICTION ON FILE: ", csvFileName)
+    
+    getActionParametersFromACsvFile(csvFileName, headerActionName, headerActionCost,
+                                    headerActionProfitPercent)
+
+    sorted_actions = sortingFusionPart_sortingAlgorithm(actions_parameters, 
+        indexOfParameterToSortBy = 0)  # Choice of sorting parameter = [0]
+    #print(sorted_actions)
+
     for action in sorted_actions:
-        if MAXIMUM_EXPENDITURE >= action[3]:
-            
+        if available_money >= action[3] and action[1] > 0:
             #buying
             bought_actions.append(action[2])
-            MAXIMUM_EXPENDITURE -= action[3]
+            available_money -= action[3]
             total_benefit += action[1]
 
-    print("bought_actions", bought_actions)
-    print("total_benefit", total_benefit) #REPRENDRE ICI : j'ai trié dans le mauvais ordre --> j'ai modifié à la ligne 86; vérifier que tout est ok t
+    print("Bought actions: ", bought_actions)
+    print("Total_benefit: ", total_benefit) 
+
+    end_total_time = time()
+    spent_total_time = end_total_time - start_total_time
+    print("Spent total time (includes opening file, etc):", spent_total_time)
 
 
-
-main()
-
+# Execute main() function once only, please, if not, available money is not 500 at the start of
+# the second execution
+#main('part1_actions.csv', "Action-#","Cout_par_action_(en_euros)","Bénéfice_(après_2_ans)")
+#main('Données_anterieures_de_Sienna/dataset1_Python+P7.csv', "name","price","profit")
+main('Données_anterieures_de_Sienna/dataset2_Python+P7.csv', "name","price","profit")
